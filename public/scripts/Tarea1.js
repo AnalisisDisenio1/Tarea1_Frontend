4eef87a19e5bcbca06723a3d6b4705ee3fcfabd0
kenystev <kjestevez.0795@gmail.com>
2016-07-25 00:55:12 -0600

    Create funcionando

--- public/scripts/Tarea1.js
+++ public/scripts/Tarea1.js
@@ -66,7 +66,95 @@ var Card = React.createClass({
   }
 });
 
+var UserForm = React.createClass({
+  getInitialState: function() {
+    return {username: '',password: ''};
+  },
+  handleUsernameChange: function(e) {
+    this.setState({username: e.target.value});
+  },
+  handlePasswordChange: function(e) {
+    this.setState({password: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var username = this.state.username.trim();
+    var password = this.state.password.trim();
+    if (!username || !password) {
+      return;
+    }
+    this.props.onUserSubmit({username: username, password: password});
+    this.setState({username: '', password: ''});
+  },
+  render: function() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="input-container">
+          <input
+            type="text"
+            id="Username"
+            name="Username"
+            required="required"
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+          />
+          <label htmlFor="Username">Username</label>
+          <div className="bar"></div>
+        </div>
+        <div className="input-container">
+          <input
+            type="password"
+            id="Password"
+            name="Password"
+            required="required"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+          <label htmlFor="Password">Password</label>
+          <div className="bar"></div>
+        </div>
+        <div className="button-container">
+          <button type="submit"><span>Save</span></button>
+        </div>
+      </form>
+    );
+  }
+});
+
+var HidenCard = React.createClass({
+  handleUserSubmit: function(user) {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: user,
+      success: function(data) {
+        console.log(data);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    return(
+      <div>
+        <div className="card"></div>
+        <div className="card">
+            <h1 className="title">{this.props.title}</h1>
+            <UserForm onUserSubmit={this.handleUserSubmit}/>
+        </div>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <Card title="List Users" url="http://localhost:3000/users" pollInterval={100}/>,
+  <Card title="List Users" url="http://9f894de3.ngrok.io/users" pollInterval={40000}/>,
   document.getElementById('content')
+);
+
+ReactDOM.render(
+  <HidenCard title="Create" url="http://9f894de3.ngrok.io/user/save" pollInterval={40000}/>,
+  document.getElementById('register')
 );
